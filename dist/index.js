9c86d76e1525f0f8d73c24bd9e1c816b665b6dde
Alfon Labadan <47669262+codepurse@users.noreply.github.com>
2022-08-15 23:04:14 +0800

    Update imports

--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,22 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+Object.defineProperty(exports, "Avatar", {
+  enumerable: true,
+  get: function get() {
+    return _avatar.default;
+  }
+});
+Object.defineProperty(exports, "AvatarBatch", {
+  enumerable: true,
+  get: function get() {
+    return _avatarBatch.default;
+  }
+});
 Object.defineProperty(exports, "Badge", {
   enumerable: true,
   get: function get() {
-    return _Badge.default;
+    return _badge.default;
   }
 });
 Object.defineProperty(exports, "Button", {
@@ -15,9 +27,77 @@ Object.defineProperty(exports, "Button", {
     return _Button.default;
   }
 });
+Object.defineProperty(exports, "Checkbox", {
+  enumerable: true,
+  get: function get() {
+    return _checkbox.default;
+  }
+});
+Object.defineProperty(exports, "CircularLoader", {
+  enumerable: true,
+  get: function get() {
+    return _CircularProgress.default;
+  }
+});
+Object.defineProperty(exports, "Header", {
+  enumerable: true,
+  get: function get() {
+    return _Header.default;
+  }
+});
+Object.defineProperty(exports, "Paragraph", {
+  enumerable: true,
+  get: function get() {
+    return _paragraph.default;
+  }
+});
+Object.defineProperty(exports, "TabContent", {
+  enumerable: true,
+  get: function get() {
+    return _tabContent.default;
+  }
+});
+Object.defineProperty(exports, "TabHeader", {
+  enumerable: true,
+  get: function get() {
+    return _tabHeader.default;
+  }
+});
+Object.defineProperty(exports, "Tabs", {
+  enumerable: true,
+  get: function get() {
+    return _tabs.default;
+  }
+});
+Object.defineProperty(exports, "Textbox", {
+  enumerable: true,
+  get: function get() {
+    return _textbox.default;
+  }
+});
 
-var _Badge = _interopRequireDefault(require("./components/Badge"));
+var _avatar = _interopRequireDefault(require("./components/avatar/avatar"));
+
+var _avatarBatch = _interopRequireDefault(require("./components/avatar/avatarBatch"));
+
+var _badge = _interopRequireDefault(require("./components/badge/badge"));
 
 var _Button = _interopRequireDefault(require("./components/button/Button"));
+
+var _checkbox = _interopRequireDefault(require("./components/checkbox/checkbox"));
+
+var _CircularProgress = _interopRequireDefault(require("./components/circularProgress/CircularProgress"));
+
+var _Header = _interopRequireDefault(require("./components/header/Header"));
+
+var _paragraph = _interopRequireDefault(require("./components/paragraph/paragraph"));
+
+var _tabContent = _interopRequireDefault(require("./components/tabs/tabContent"));
+
+var _tabHeader = _interopRequireDefault(require("./components/tabs/tabHeader"));
+
+var _tabs = _interopRequireDefault(require("./components/tabs/tabs"));
+
+var _textbox = _interopRequireDefault(require("./components/text/textbox"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,27 @@
-import Badge from "./components/Badge";
+import Avatar from "./components/avatar/avatar";
+import AvatarBatch from "./components/avatar/avatarBatch";
+import Badge from "./components/badge/badge";
 import Button from "./components/button/Button";
+import Checkbox from "./components/checkbox/checkbox";
+import CircularLoader from "./components/circularProgress/CircularProgress";
+import Header from "./components/header/Header";
+import Paragraph from "./components/paragraph/paragraph";
+import TabContent from "./components/tabs/tabContent";
+import TabHeader from "./components/tabs/tabHeader";
+import Tabs from "./components/tabs/tabs";
+import Textbox from "./components/text/textbox";
 
-export { Badge, Button };
+export {
+  Badge,
+  Button,
+  Avatar,
+  AvatarBatch,
+  Checkbox,
+  CircularLoader,
+  Header,
+  Paragraph,
+  Textbox,
+  TabContent,
+  TabHeader,
+  Tabs,
+};
