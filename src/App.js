b5169a7dc77bcd6f31c0095a025fd3e48c2239b0
Alfon Labadan <47669262+codepurse@users.noreply.github.com>
2022-10-17 22:02:47 +0800

    Update package version

--- src/App.js
+++ src/App.js
@@ -1,84 +1,46 @@
-import { MdSort } from "react-icons/md";
 import "./App.css";
+import Box from "./lib/components/box/box";
+import Button from "./lib/components/button/Button";
 import Checkbox from "./lib/components/checkbox/checkbox";
 import Overlay from "./lib/components/overlay/overlay";
-import Space from "./lib/components/space/space";
 import "./lib/components/styles/main.scss";
 import TabContent from "./lib/components/tabs/tabContent";
 import TabHeader from "./lib/components/tabs/tabHeader";
 import Tabs from "./lib/components/tabs/tabs";
+import Textbox from "./lib/components/text/textbox";
 function App() {
-  const columns = [
-    {
-      key: "bank",
-      headerName: () => (
-        <Space gap={10}>
-          <p>Header Name</p>
-          <Overlay
-            rendered={
-              <div className="sampleOverlay">
-                <Space gap={10} direction="column">
-                  <Checkbox label="Test" positionLabel="right"></Checkbox>
-                  <Checkbox label="Test" positionLabel="right"></Checkbox>
-                </Space>
-              </div>
-            }
-          >
-            <i>
-              <MdSort />
-            </i>
-          </Overlay>
-        </Space>
-      ),
-    },
-    {
-      key: "suitability",
-      headerName: "Suitability",
-    },
-    {
-      key: "customer",
-      headerName: "Customer",
-    },
-    {
-      key: "volume",
-      headerName: "Credit Volume",
-    },
-    {
-      key: "action",
-      headerName: "",
-    },
-  ];
-
-  const data = [
-    {
-      bank: "Anz Banking",
-      suitability: "Pairing",
-      customer: "Michael Polansky",
-      volume: "$300,000.00",
-    },
-    {
-      bank: "Axis Bank",
-      suitability: "Fragmentary",
-      customer: "David Hoffman",
-      volume: "$500,000.00",
-    },
-    {
-      bank: "ING Group",
-      suitability: "Pairing",
-      customer: "Helen Howard",
-      volume: "$100,000.00",
-    },
-  ];
-
   return (
     <>
       <div style={{ width: "800px" }}>
-        <Tabs defaultKey="3" mb="0">
-          <TabHeader label="Tab One" id="3"></TabHeader>
-          <TabHeader label="Tab Two" id="4"></TabHeader>
-          <TabContent id="3"></TabContent>
-          <TabContent id="4"></TabContent>
-        </Tabs>
+        <Overlay
+          style={{ backgroundColor: "transparent", boxShadow: "none" }}
+          rendered={
+            <Tabs defaultKey="3" mb="0">
+              <TabHeader label="Tab One" id="3"></TabHeader>
+              <TabHeader label="Tab Two" id="4"></TabHeader>
+              <TabContent id="3">
+                <Textbox fill placeholder="Search here.."></Textbox>
+                <Box mt="15px">
+                  <Checkbox label="Textbook"></Checkbox>
+                </Box>
+                <Box mt="15px">
+                  <Checkbox label="Learning application"></Checkbox>
+                </Box>
+                <Box mt="15px">
+                  <Checkbox label="Additonal material"></Checkbox>
+                </Box>
+                <Box mt="15px">
+                  <Checkbox label="No type set"></Checkbox>
+                </Box>
+              </TabContent>
+              <TabContent id="4">
+                <p style={{ fontSize: "2rem" }}> 😆 </p>
+              </TabContent>
+            </Tabs>
+          }
+        >
+          <Button>Add filter</Button>
+        </Overlay>
       </div>
     </>
   );
