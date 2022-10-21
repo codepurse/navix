d40b28da437a54c749a4aa49fcbb1c0e1f234bb4
Alfon Labadan <47669262+codepurse@users.noreply.github.com>
2022-10-21 12:37:20 +0800

    Revamp badge component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Badge from "./lib/components/badge/badge";
+import Button from "./lib/components/button/Button";
 import "./lib/components/styles/main.scss";
 function App() {
   return (
     <>
       <div style={{ width: "800px" }}>
-        <Badge type="danger">Neutral</Badge>
+        <Button type="primary" style={{ position: "relative" }}>
+          Profile <Badge type="danger">aasrasdsad</Badge>
+        </Button>
       </div>
     </>
   );
--- src/lib/components/badge/badge.js
+++ src/lib/components/badge/badge.js
@@ -3,12 +3,48 @@ import { PropTypes } from "prop-types";
 import React from "react";
 import Box from "../box/box";
 import { badgeType } from "./badgeFunction";
-const BADGE_TYPE = ["success", "danger", "warning", "info", "neutral"];
 
+const BADGE_TYPE = ["success", "danger", "warning", "info", "neutral"];
+const BADGE_POSITION = ["top-right", "top-left", "bottom-right", "bottom-left"];
 Badge.propTypes = {
   type: PropTypes.oneOf(BADGE_TYPE),
+  position: PropTypes.oneOf(BADGE_POSITION),
 };
 
+const positionType = (value) => {
+  switch (value) {
+    case "top-right":
+      return {
+        position: "absolute",
+        top: "0",
+        left: "100%",
+        transform: "translate(-50%,-50%)",
+      };
+    case "bottom-right":
+      return {
+        position: "absolute",
+        top: "100%",
+        left: "100%",
+        transform: "translate(-50%,-50%)",
+      };
+    case "top-left":
+      return {
+        position: "absolute",
+        top: "0",
+        left: "0",
+        transform: "translate(-50%,-50%)",
+      };
+    case "bottom-left":
+      return {
+        position: "absolute",
+        top: "100%",
+        left: "0",
+        transform: "translate(-50%,-50%)",
+      };
+    default:
+      return;
+  }
+};
 export default function Badge(props) {
   const badgeClassName = classNames(
     "nvxBadge",
@@ -16,7 +52,10 @@ export default function Badge(props) {
     badgeType(props.type)
   );
   return (
-    <Box className={badgeClassName} style={props.style}>
+    <Box
+      className={badgeClassName}
+      style={(props.style, positionType(props.position))}
+    >
       {props.children}
     </Box>
   );
